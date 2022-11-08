a8eefe2205ea6f255503cff937f85013899debdb
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-08 20:12:36 -0300

    1

--- empresa/views.py
+++ empresa/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tecnologias
 
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == "GET":
+        techs = Tecnologias.objects.all()
+        return render(request, 'nova_empresa.html', {'techs': techs})
+    elif request.method == "POST":
+        return HttpResponse("Tô aqui")
