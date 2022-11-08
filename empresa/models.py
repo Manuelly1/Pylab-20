60f1bbe65ababc6b2f590f87208b07d0cefab415
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-08 19:29:34 -0300

    aula 1

--- empresa/models.py
+++ empresa/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tecnologia
+    
 class Empresa(models.Model):
     choices_nicho_mercado = (
         ('M', 'Marketing'),
         ('N', 'Nutrição'),
         ('D', 'Design')
     )
+    logo = models.ImageField(upload_to="logo_empresa", null=True)
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     cidade = models.CharField(max_length=30)
+    tecnologias = models.ManyToManyField(Tecnologias)
     endereco = models.CharField(max_length=30)
     caracteristica_empresa = models.TextField()
     nicho_mercado = models.CharField(max_length=3, choices=choices_nicho_mercado)
+
+    def __str__(self):
+        return self.nome
