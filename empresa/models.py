ab675fda0ac1a411753de391a51387162fc8bd61
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-08 11:18:40 -0300

    aula 1

--- empresa/models.py
+++ empresa/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Empresa(models.Model):
+    choices_nicho_mercado (
+        ('M', 'Marketing')
+        ('N', 'Nutrição')
+        ('D', 'Design')
+    )
     nome = models.CharField(max_length=30)
-    email = models.CharField()
+    email = models.EmailField()
     cidade = models.CharField(max_length=30)
-    endereco = models.CharField(max_Length=30)
+    endereco = models.CharField(max_length=30)
     caracteristica_empresa = models.TextField()
+    nicho_mercado = models.CharField(max_length=30)
