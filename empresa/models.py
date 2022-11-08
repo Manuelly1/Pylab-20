621b7e808e036e0b4bc7c74e097f5c406b80426e
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-08 19:45:22 -0300

    first day

--- empresa/models.py
+++ empresa/models.py
@@ -23,3 +23,29 @@ class Empresa(models.Model):
 
     def __str__(self):
         return self.nome
+class Vagas(models.Model):
+    choices_experiencia = (
+        ('J', 'Júnior'),
+        ('P', 'Pleno'),
+        ('S', 'Sênior')
+    )
+
+    choices_status = (
+        ('I', 'Interesse'),
+        ('C', 'Currículo enviado'),
+        ('E', 'Entrevista'),
+        ('D', 'Desafio técnico'),
+        ('F', 'Finalizado')
+    )
+    
+    empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=30)
+    nivel_experiencia = models.CharField(max_length=2, choices=choices_experiencia)
+    data_final = models.DateField()
+    status = models.CharField(max_length=30, choices=choices_status)
+    tecnologias_dominadas = models.ManyToManyField(Tecnologias)
+    tecnologias_estudar = models.ManyToManyField(Tecnologias, related_name='estudar')
+
+
+    def __str__(self):
+        return self.titulo
